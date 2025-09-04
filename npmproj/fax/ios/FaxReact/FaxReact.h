092b823f97adaf9763b4b2347635d31a42f7bb76
sainath.kodiganti <sainath.kodiganti@merkle.com>
2025-09-04 14:36:45 +1000

    feat: proj files

---
+++ npmproj/fax/ios/FaxReact/FaxReact.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for FaxReact.
+FOUNDATION_EXPORT double FaxReactVersionNumber;
+
+//! Project version string for FaxReact.
+FOUNDATION_EXPORT const unsigned char FaxReactVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaxReact/PublicHeader.h>
